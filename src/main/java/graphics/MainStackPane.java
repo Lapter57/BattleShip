d695bb628664c8a5aab81f89727bbd522915fda8
Egor Shakhmin <lapter57@gmail.com>
2021-07-08 13:09:02 +0300

    switch to h2 db (#4) (#5)

--- src/main/java/graphics/MainStackPane.java
+++ src/main/java/graphics/MainStackPane.java
@@ -12,7 +12,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class MainStPain extends StackPane{
+public class BattleShipStackPane extends StackPane {
     Graphic.Title title = new Graphic.Title("B A T T L E S H I P");
     {
         title.setTranslateX(-20);
@@ -34,7 +34,7 @@ public class MainStPain extends StackPane{
 
     Graphic.MenuBox menuBox = new Graphic.MenuBox(mainMenu);
 
-    MainStPain() {
+    BattleShipStackPane() {
         setMaxWidth(1280);
         setMinHeight(720);
         try (InputStream is = Files.newInputStream(Paths.get("src/main/resources/background/BattleShip.jpg"))) {
